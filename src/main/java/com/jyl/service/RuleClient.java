a77a0ead247107d4d66292d248b44fe55ca3fd53
huangjing <sn-huangjing@pptv.com>
2019-05-14 20:14:14 +0800

    修改循环为1次

--- src/main/java/com/jyl/service/RuleClient.java
+++ src/main/java/com/jyl/service/RuleClient.java
@@ -36,7 +36,7 @@ public class RuleClient {
         Facts facts = new Facts();
         StringBuffer result = new StringBuffer();
         facts.put("result", result);
-        for (int i = 1; i <= 2; i++) {
+        for (int i = 1; i <= 1; i++) {
             facts.put("number", 35);
             facts.put("othernumber", 35);
             fizzBuzzEngine.fire(rules, facts);
